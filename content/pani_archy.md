84e1e1374c1d449362fb95bb93f80dd0b5b39251
Marrek Nožka <marek@tlapicka.net>
2018-06-07 10:51:20 +0200

    první

---
+++ content/pani_archy.md
@@ -0,0 +1,40 @@
+Title: Naše Matka Maria, naše Paní Archy
+Date: 2018-06-07 09:24
+Category: Blog
+Tags: Maria
+Author: Marek Nožka
+Summary: V Noemově době se záchranou pro lidstvo stala archa.
+         V naší době je archou pro naši záchranu Neposkvrněné Srdce Panny Marie.
+
+V roce 1991 vydalo Mariánské nakladatelství v Brně soubor  duchovních naučení,
+rozjímání a modlitby z různých novodobých zjevení Ježíšových a Mariiných, které
+vybral a přeložil František Press a má název Mariin Nebeklíč.
+
+Z této knihy si dovolím ocitovat.
+
+  Obracím se na vás se zvláštní žádostí, abyste mě 13. dne každého měsíce
+  uctívali, abyste z tohoto dne pro mne učinili svatý den, abyste se v tento
+  den lásky modlili, činili pokání a oběti, abyste mě uctívali pod titulem -
+  **Naše Paní Archy, naše Matka Maria, Ochránkyně křesťanů**. Žádám vás
+  všechny, komu je to možné, abyste se toho dne v poledne pomodlili svatý
+  růženec, a budete-li mě pod tímto titulem vzývat, slibuji vám, mé děti, že
+  požádáte-li mě a nebeského Otce v tuto hodinu a při vzývání uvedeného titulu
+  o jakoukoli milost, obdržíte ji. Tento titul,mé děti, má na nebi velikou
+  moc. -Maria v Novře 1984-
+
+V Noemově době se záchranou pro lidstvo stala Noemova archa. V naší době je
+archou pro naši záchranu Neposkvrněné Srdce Panny Marie. Panna Maria mnohokrát
+a na mnoha místech stále připomíná jak je potřebná modlitba a půst. 
+
+
+Fatima
+------
+
+* <https://www.slavne-dny.cz/episode/618301/den-kdy-tisice-lidi-videly-zazrak-13-rijen>
+* <http://www.fatym.com/view.php?nazevclanku=doporucujeme-de-mattei-abychom-zachranili-cirkev-musime-zasvetit-rusko-neposkvrnenemu-srdci-panny-marie&cisloclanku=2017040130>
+
+Medžugorie a Fatima
+----------------------
+
+* <https://youtu.be/RNnYvYnngs8>
+* <https://youtu.be/zULjFcqL_Ns>
---
+++ pelicanconf.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Marek Nožka'
+SITENAME = u'Marrek.cz'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Prague'
+
+DEFAULT_LANG = u'cs'
+
+THEME = 'theme-simplegray/'
+
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ('www.jezisuzdravuje.cz', 'http://www.jezisuzdravuje.cz'),
+    ('www.vojtechkodet.cz', 'http://www.vojtechkodet.cz'),
+    ('Milujte se', 'http://milujte.se'),
+    ('Ron Wyatt', 'http://www.b-a-n.cz/informace.html'),
+    ('Stvoření?', 'http://www.stvoreni.cz/'),
+    ('Manželství.cz', 'http://www.manzelstvi.cz/'),
+    ('Maria.cz', 'http://www.maria.cz/'),
+    ('Jsem pro život', 'http://prolife.cz/'),
+    ('Kurzy α', 'http://www.kurzyalfa.cz/'),
+    ('MatPlotLib', 'http://matplotlib.org'),
+    ('NumPy', 'http://www.numpy.org/'),
+    ('SciPy', 'http://scipy.org/'),
+    ('Zim - A Desktop Wiki', 'http://zim-wiki.org/'),
+    ('VOŠ a SPŠe Olomouc', 'http://www.spseol.cz/'),
+    ('Antispam', 'http://antispam.er.cz/'),
+    # ('Check I/O', 'http://www.checkio.org/'),
+    ('Oprava trekové obuvy', 'http://www.trekovaobuv.eu/'),
+    ('searchcode.com', 'https://searchcode.com/'),
+    # ('', ''),
+)
+# Social widget
+SOCIAL = (
+    ('Github', 'http://github.com/MarrekNozka'),
+    ('Twitter', 'http://twitter.com/MarrekNozka'),
+    ('YouTube', 'https://www.youtube.com/user/YouTlapickaTube'),
+    ('Flickr', 'http://www.flickr.com/photos/tlapicka/'),
+    ('Wikipedista', 'http://cs.wikipedia.org/wiki/Wikipedista:Tlapicka'),
+    ('Wikiobčan',
+        'http://commons.wikimedia.org/wiki/Special:ListFiles/Tlapicka'),
+    ('Wikispisovatel', 'http://cs.wikibooks.org/wiki/User:Tlapicka'),
+    ('Mamut', 'http://mamut.spseol.cz/nozka/'),
+)
+
+DEFAULT_PAGINATION = 20
+
+MD_EXTENSIONS = [
+    'codehilite(css_class=highlight)',
+    'extra',
+    'headerid(level=2)',
+    'toc',
+    'linksShortcuts:Shortcuts',
+]
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
